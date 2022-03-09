7171ab4dd1a5e8ea930ccd56317df52b456ee607
Никита Скулыбердин <n.skul2012@mail.ru>
2022-03-09 19:50:08 +0300

    CTPS-41 Добавлен класс UserRole + рефакторинг

--- src/main/java/com/validation/model/User.java
+++ src/main/java/com/validation/model/User.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 public class User {
 
     @Id
--- src/main/java/com/validation/model/UserRole.java
+++ src/main/java/com/validation/model/UserRole.java
@@ -1,4 +1,16 @@
 package com.validation.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table (name = "user_roles")
 public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
 }
